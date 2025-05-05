fbb8bb48c2b041b92465e09a24bfd4b5a88c478d
Numulix <jbabic999@gmail.com>
2025-05-05 18:40:18 +0200

    refactor(auth): use react hook form for login/register forms

--- src/components/auth/Register.tsx
+++ src/components/auth/Register.tsx
@@ -1,103 +1,64 @@
 import React, { useState } from 'react'
 import supabase from '../../supabase/supabase';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
-interface RegisterErrors {
-    username?: string;
-    email?: string;
-    password?: string;
-    confirmPassword?: string;
-    supabase?: string;
+
+type RegisterInputs = {
+    username: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
 }
 
 function Register() {
-    const [username, setUsername] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [error, setError] = useState<RegisterErrors>({
-        username: undefined,
-        email: undefined,
-        password: undefined,
-        confirmPassword: undefined
-    });
-    const [isSubmitting, setIsSubmitting] = useState(false);
-
-    // TODO: Use React Hook Form for form handling and validation - this is just temporary
-    const handleSubmit = async (e: React.FormEvent) => {
-        e.preventDefault();
-        setError({}); // Reset errors
-        setIsSubmitting(true); // Set submitting state
-
-        const newErrors: RegisterErrors = {};
-        
-        if (!username.trim()) {
-            newErrors.username = 'Username is required';
-        }
-
-        if (!email.trim()) {
-            newErrors.email = 'Email is required';
-        } else if (!/\S+@\S+\.\S+/.test(email)) {
-            newErrors.email = 'Email address is invalid';
-        }
-        if (!password.trim()) {
-            newErrors.password = 'Password is required';
-        } else if (password.length < 6) {
-            newErrors.password = 'Password must be at least 6 characters';}
-        if (!confirmPassword.trim()) {
-            newErrors.confirmPassword = 'Confirm Password is required';
-        } else if (confirmPassword !== password) {
-            newErrors.confirmPassword = 'Passwords do not match';
-        }
-        if (Object.keys(newErrors).length > 0) {
-            setError(newErrors);
-            setIsSubmitting(false);
-            return;
-        }
-
-        try {
-            const { data, error } = await supabase.auth.signUp({
-                email,
-                password,
-                options: {
-                    data: {
-                        username
-                    }
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors }
+    } = useForm<RegisterInputs>({});
+    const onSubmit: SubmitHandler<RegisterInputs> = async (data) => {
+        setIsSubmitting(true);
+        const { error } = await supabase.auth.signUp({
+            email: data.email,
+            password: data.password,
+            options: {
+                data: {
+                    username: data.username
                 }
-            });
-
-            if (error) {
-                setError({ supabase: error.message });
-            } else {
-                alert('Registration successful! Please check your email for a confirmation link.');
             }
-        } catch (error) {
-            setError({ supabase: 'An error occurred while creating your account. Please try again.' });
-        } finally {
-            setIsSubmitting(false);
+        })
+        if (error) {
+            console.error(error);
+            alert("Error: " + error.message);
+        } else {
+            alert("Check your email for the confirmation link!");
         }
+        setIsSubmitting(false);
     }
+    const [isSubmitting, setIsSubmitting] = useState(false);
 
   return (
     <div className='mt-8 flex flex-row justify-center items-center'>
-        <form className='w-full max-w-sm' onSubmit={handleSubmit}>
+        <form className='w-full max-w-sm' onSubmit={handleSubmit(onSubmit)}>
             <fieldset className='fieldset bg-base-100 border-base-300 rounded-box border p-4'>
                 <legend className='fieldset-legend text-lg'>Register</legend>
 
                 <label className='label'>Username</label>
-                <input type="text" placeholder='e.g. PikachuLover93' className='input w-full' onChange={(e) => setUsername(e.target.value)} />
-                {error.username && <p className='text-red-500 text-sm'>{error.username}</p>}
+                <input type="text" placeholder='e.g. PikachuLover93' className='input w-full' {...register("username", { required: "Please enter your username" })} />
+                {errors.username && <p className='text-red-500 text-sm'>{errors.username.message}</p>}
 
                 <label className='label'>Email</label>
-                <input type="email" placeholder='Email' className='input w-full' onChange={(e) => setEmail(e.target.value)} />
-                {error.email && <p className='text-red-500 text-sm'>{error.email}</p>}
+                <input type="email" placeholder='Email' className='input w-full' {...register("email", { required: "Please enter your email" })} />
+                {errors.email && <p className='text-red-500 text-sm'>{errors.email.message}</p>}
 
                 <label className='label'>Password</label>
-                <input type="password" placeholder='Password' className='input w-full' onChange={(e) => setPassword(e.target.value)} />
-                {error.password && <p className='text-red-500 text-sm'>{error.password}</p>}
+                <input type="password" placeholder='Password' className='input w-full' {...register("password", { required: "Please enter your password", minLength: { value: 8, message: "Your password must be at least 8 characters long" } })} />
+                {errors.password && <p className='text-red-500 text-sm'>{errors.password.message}</p>}
 
                 <label className='label'>Confirm Password</label>
-                <input type="password" placeholder='Confirm Password' className='input w-full' onChange={(e) => setConfirmPassword(e.target.value)} />
-                {error.confirmPassword && <p className='text-red-500 text-sm'>{error.confirmPassword}</p>}
+                <input type="password" placeholder='Confirm Password' className='input w-full' {...register("confirmPassword", { required: "Please confirm your password", validate: value => value === watch('password') || "Passwords do not match" })} />
+                {errors.confirmPassword && <p className='text-red-500 text-sm'>{errors.confirmPassword.message}</p>}
 
                 <button type='submit' disabled={isSubmitting} className='btn btn-neutral mt-4'>{isSubmitting ? (<span className='loading loading-spinner loading-xs'></span>) : 'Register' }</button>
                 <p className='text-sm text-center mt-2'>Already have an account? <a href="/login" className='link'>Login</a></p>
